bd5ce6f9d44136017d4abde0c5f383558c82b1a4
Jean-Philippe Bernardy <jean-philippe.bernardy@tweag.io>
2016-01-31 09:14:29 +0100

    update to new lp-diagrams

--- examples/Main.hs
+++ examples/Main.hs
@@ -1,41 +1,48 @@
 import Graphics.Diagrams.SVG
-
+import Algebra.Classes
+import Prelude hiding (Num(..))
 -- autoLab s i = do
---   o <- labelObj s
+--   o <- label s
 --   -- stroke "red" $ path $ polyline [o # BaseW, o # BaseE]
 --   -- stroke "blue" $ path $ polygon [o # NW, o # NE, o # SE, o # SW]
 --   autoLabelObj o i
 
-autoLab = autoLabel
+label' x = label x x
+autoLabel' x = autoLabel x x
 
 testDiagram :: SvgDiagram ()
 testDiagram = do
-  -- draw $ path $ circle (Point 0 0) 5
-  a   <- labelObj $ "ha"
-  b   <- labelObj $ "b"
-  a'  <- draw $ circleShape -- labelObj $ ensureMath $ "c"
-  width a' === 15
-  b'  <- labelObj $ "b'"
-  a'' <- labelObj $ "."
-  b'' <- labelObj $ "."
+  l1 <- label' "whatever1"
+  l2 <- label' "whatever2"
+  registerNonOverlap (l1#SW) (l1#NE)
+  registerNonOverlap (l2#SW) (l2#NE)
+  a   <- label' $ "ha"
+
+  b   <- label' $ "b"
+  a'  <- draw $ circle "circleEx" -- label $ ensureMath $ "c"
+  width a' === constant 15
+  b'  <- label' $ "b'"
+  a'' <- label' $ "."
+  b'' <- label' $ "."
 
   -- c <- texObj $ ensureMath $ "c"
   -- Center ▸ c === MP.center [E ▸ a'', E ▸ b''] + (20 +: 0)
 
-  let width = 70
-  vdist b a === 30
+  let width = constant 70
+  vdist b a === constant 30
   hdist a a' === width
   hdist a' a'' === width
   alignMatrix $ map (map (#Center)) $ [[a,a',a'']
                                       ,[b, b',b'']]
-  autoLab "aa'" =<< arrow a a'
-  autoLab "bb'" =<< arrow b b'
-  autoLab "ab" . turn180 =<< arrow a b
-  autoLab "a'a''" =<< arrow a' a''
-  autoLab "b'b''" =<< arrow b' b''
+  autoLabel' "aa'" =<< arrow a a'
+  noOverlap =<< autoLabel' "bb'" =<< arrow b b'
+  autoLabel' "ab" . turn180 =<< arrow a b
+  autoLabel' "a'a''" =<< arrow a' a''
+  autoLabel' "b'b''" =<< arrow b' b''
+
 
   draw $ do
-    autoLab "equal" =<< edge a'' b''
+    autoLabel' "equal" =<< edge a'' b''
   return ()
 
 main :: IO ()
